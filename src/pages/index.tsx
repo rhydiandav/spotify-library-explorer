b56172824e2c5b65c87c9645387e46ac6f0cd9b8
Rhydian Davies <rhydian@Rhydians-MacBook-Air.local>
2023-02-18 14:52:52 +0000

    display a list of album titles from user library

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import { LOGIN_URL } from "consts";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 type HomeInitialProps = {
   query: {
@@ -12,6 +14,23 @@ type HomeProps = {
 };
 
 export default function Home({ access_token }: HomeProps) {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.spotify.com/v1/me/albums", {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        setAlbums(response.data.items);
+      });
+  }, []);
+
+  console.log(albums);
+
   return (
     <>
       <Head>
@@ -24,6 +43,14 @@ export default function Home({ access_token }: HomeProps) {
         ) : (
           <a href={LOGIN_URL}>Login with Spotify</a>
         )}
+        {albums.length &&
+          albums.map(({ album }) => {
+            return (
+              <div key={album.id}>
+                <p>{album.name}</p>
+              </div>
+            );
+          })}
       </main>
     </>
   );
